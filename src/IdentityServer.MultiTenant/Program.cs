d2a6e4eac2d4bda780a3b042e7eb4a1208cda624
Joe Walters <info@thenakeddeveloper.com>
2022-12-10 17:02:32 +0000

    Added in missing FinBuckle packages

--- src/IdentityServer.MultiTenant/Program.cs
+++ src/IdentityServer.MultiTenant/Program.cs
@@ -1,3 +1,5 @@
+using Finbuckle.MultiTenant;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Finbuckle multi-tenant handling
+builder.Services.AddMultiTenant<TenantInfo>()
+                .WithHostStrategy()
+                .WithConfigurationStore();
 
 var app = builder.Build();
 
