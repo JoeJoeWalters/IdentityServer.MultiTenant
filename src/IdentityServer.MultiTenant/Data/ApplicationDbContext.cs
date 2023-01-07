92bd867eab3bef17ece82dd1e1afe9fdeb2475a4
Joe Walters <info@thenakeddeveloper.com>
2023-01-07 00:46:51 +0000

    Attempt 3

---
+++ src/IdentityServer.MultiTenant/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Entities;
+using Finbuckle.MultiTenant;
+using Finbuckle.MultiTenant.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.MultiTenant.Data
+{
+    // https://www.finbuckle.com/MultiTenant/Docs/v6.9.1/EFCore
+    // https://github.com/AndrewTriesToCode/MultiTenantIdentityServer4/blob/main/src/IdentityServerAspNetIdentity/Data/MultiTenantConfigurationDbContext.cs
+    public class ApplicationDbContext : MultiTenantIdentityDbContext
+    {
+        public ApplicationDbContext(ITenantInfo tenantInfo) : base(tenantInfo)
+        {
+        }
+
+        public ApplicationDbContext(ITenantInfo tenantInfo, DbContextOptions<ApplicationDbContext> options) :
+            base(tenantInfo, options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //builder.Entity<ApplicationUser>().IsMultiTenant();
+        }
+    }
+}
--- src/IdentityServer.MultiTenant/Data/MultiConfigurationDbContext.cs
+++ src/IdentityServer.MultiTenant/Data/MultiConfigurationDbContext.cs
@@ -1,27 +1,53 @@
 ﻿using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Entities;
 using Finbuckle.MultiTenant;
+using Finbuckle.MultiTenant.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityServer.MultiTenant.Data
 {
     // https://www.finbuckle.com/MultiTenant/Docs/v6.9.1/EFCore
     // https://github.com/AndrewTriesToCode/MultiTenantIdentityServer4/blob/main/src/IdentityServerAspNetIdentity/Data/MultiTenantConfigurationDbContext.cs
-    public class MultiConfigurationDbContext : ConfigurationDbContext<MultiConfigurationDbContext>
+    public class MultiConfigurationDbContext : ConfigurationDbContext<MultiConfigurationDbContext>, IMultiTenantDbContext
     {
-        private ITenantInfo _tenantInfo;
-
         public MultiConfigurationDbContext(DbContextOptions<MultiConfigurationDbContext> options) : base(options)
         {
         }
 
         public MultiConfigurationDbContext(ITenantInfo tenantInfo, DbContextOptions<MultiConfigurationDbContext> options) : base(options)
         {
-            _tenantInfo = tenantInfo;   
+            TenantInfo = tenantInfo;
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Client>().IsMultiTenant();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            this.EnforceMultiTenant();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            this.EnforceMultiTenant();
+            return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public ITenantInfo TenantInfo { get; }
+
+        public TenantMismatchMode TenantMismatchMode { get; } = TenantMismatchMode.Throw;
+
+        public TenantNotSetMode TenantNotSetMode { get; } = TenantNotSetMode.Throw;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseInMemoryDatabase(_tenantInfo.ConnectionString ?? throw new InvalidOperationException());
+            optionsBuilder.UseInMemoryDatabase(TenantInfo.ConnectionString ?? throw new InvalidOperationException());
             //optionsBuilder.UseSqlite(TenantInfo.ConnectionString ?? throw new InvalidOperationException());
             base.OnConfiguring(optionsBuilder);
         }
